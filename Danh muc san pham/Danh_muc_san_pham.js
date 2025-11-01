11dee5b50881ce79c55752038166dd9be5b9a922
phanhongphuc-sys <23130249@st.hcmuaf.edu.vn>
2025-11-01 21:40:11 +0700

    Tạo trang danh mục sản phẩm

---
+++ Danh muc san pham/Danh_muc_san_pham.js
@@ -0,0 +1,14 @@
+// Lấy tất cả các menu
+const sideMenus = document.querySelectorAll('.side-menu');
+
+// Lặp qua từng menu
+sideMenus.forEach(menu => {
+    const sideTitle = menu.querySelector('.side-title');
+    const sideBody = menu.querySelector('.side-body');
+
+    // Gắn sự kiện click cho từng menu riêng
+    sideTitle.addEventListener('click', () => {
+        sideTitle.classList.toggle('active');
+        sideBody.classList.toggle('show');
+    });
+});
