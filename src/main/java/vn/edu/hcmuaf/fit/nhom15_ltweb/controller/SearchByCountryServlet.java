75ee32a7b880bd4c50ec3db5bf85f5e39b0b084c
Doãn Trần Đình Khang <23130143@st.hcmuaf.edu.vn>
2026-01-24 22:03:00 +0700

    lam tiep danh muc

--- src/main/java/vn/edu/hcmuaf/fit/nhom15_ltweb/controller/SearchByCountryServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/nhom15_ltweb/controller/SearchByCountryServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import vn.edu.hcmuaf.fit.nhom15_ltweb.model.TourWithImage;
+import vn.edu.hcmuaf.fit.nhom15_ltweb.service.TourService;
 import vn.edu.hcmuaf.fit.nhom15_ltweb.ultils.DBConnect;
 
 import java.io.IOException;
@@ -19,40 +21,13 @@ import java.util.List;
 
 @WebServlet("/search-by-country")
 public class SearchByCountryServlet extends HttpServlet {
+    private TourService tourService = new TourService();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String country = request.getParameter("country");
-        List<Tour> tourList = TourDAO.getToursByCountry(country);
-
+        List<TourWithImage> tourList = tourService.getToursWithImageByCountry(country); // DAO mới
         request.setAttribute("tourList", tourList);
-        request.setAttribute("country", country);
-
         request.getRequestDispatcher("/Danh_muc_san_pham.jsp").forward(request, response);
-    }
-    public static List<Tour> getToursByCountryWithImage(String country) {
-        List<Tour> tours = new ArrayList<>();
-        String sql = "SELECT t.*, ti.imageURL " +
-                "FROM tours t " +
-                "LEFT JOIN ( " +
-                "    SELECT tourID, MIN(imageID) as minImageID " +
-                "    FROM tourimages GROUP BY tourID " +
-                ") minimg ON t.tourID = minimg.tourID " +
-                "LEFT JOIN tourimages ti ON minimg.minImageID = ti.imageID " +
-                "WHERE t.location = ?";
-        try (Connection conn = DBConnect.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, country);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Tour tour = new Tour();
-                // ...các dòng set trường... copy hệt trên!
-//                tour.setImageURL(rs.getString("imageURL"));
-                tours.add(tour);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return tours;
     }
 }
